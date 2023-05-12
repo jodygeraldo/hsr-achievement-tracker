0d956c848239d9cd26705de530186827edca938b
Jody Geraldo <jody.geraldo.jg@gmail.com>
2023-05-12 17:04:02 +0700

    fix: undefined userPrefs fail

--- app/utils/user-prefs.server.ts
+++ app/utils/user-prefs.server.ts
@@ -1,7 +1,8 @@
 import { createCookie } from "@remix-run/cloudflare"
+import type { Infer } from "superstruct"
 import { boolean, create, defaulted, object, type } from "superstruct"
 
-const UserPrefs = type({
+const userPrefsSchema = type({
 	showMissedFirst: defaulted(boolean(), false),
 	showClue: object({
 		normalAchievement: defaulted(boolean(), false),
@@ -9,13 +10,22 @@ const UserPrefs = type({
 	}),
 })
 
+type UserPrefs = Infer<typeof userPrefsSchema>
+
 const userPrefsCookie = createCookie("user-prefs")
 
-async function getUserPrefs(request: Request) {
+async function getUserPrefs(request: Request): Promise<UserPrefs> {
+	const defaultUserPrefs: UserPrefs = {
+		showMissedFirst: false,
+		showClue: {
+			normalAchievement: false,
+			secretAchievement: false,
+		},
+	}
 	const cookieHeader = request.headers.get("Cookie")
-	const cookie = (await userPrefsCookie.parse(cookieHeader)) ?? undefined
+	const cookie = (await userPrefsCookie.parse(cookieHeader)) ?? defaultUserPrefs
 
-	return create(cookie, UserPrefs)
+	return create(cookie, userPrefsSchema)
 }
 
 export { userPrefsCookie, getUserPrefs }
