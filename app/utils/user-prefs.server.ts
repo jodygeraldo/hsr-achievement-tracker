c04e79d481a1d1c63991b3f9c343f851971346ba
Jody Geraldo <jody.geraldo.jg@gmail.com>
2023-05-16 13:26:49 +0700

    refactor: improved handling of invalid userPrefs object for better resilience

--- app/utils/user-prefs.server.ts
+++ app/utils/user-prefs.server.ts
@@ -1,46 +1,84 @@
 import { createCookie } from "@remix-run/cloudflare"
 import type { Infer } from "superstruct"
-import { boolean, coerce, defaulted, mask, object } from "superstruct"
+import {
+	StructError,
+	boolean,
+	enums,
+	is,
+	mask,
+	object,
+	type,
+} from "superstruct"
 
-const userPrefsSchema = object({
-	showMissedFirst: defaulted(boolean(), true),
-	showClue: defaulted(
-		object({
-			normalAchievement: defaulted(
-				coerce(
-					object({
-						beforeAchieved: defaulted(boolean(), true),
-						afterAchieved: defaulted(boolean(), true),
-					}),
-					boolean(),
-					(value) => ({ beforeAchieved: value, afterAchieved: value })
-				),
-				{}
-			),
-			secretAchievement: defaulted(
-				coerce(
-					object({
-						beforeAchieved: defaulted(boolean(), false),
-						afterAchieved: defaulted(boolean(), true),
-					}),
-					boolean(),
-					(value) => ({ beforeAchieved: value, afterAchieved: value })
-				),
-				{}
-			),
+const userPrefsStruct = type({
+	showMissedFirst: boolean(),
+	showClue: object({
+		normalAchievement: object({
+			beforeAchieved: boolean(),
+			afterAchieved: boolean(),
 		}),
-		{}
-	),
+		secretAchievement: object({
+			beforeAchieved: boolean(),
+			afterAchieved: boolean(),
+		}),
+	}),
 })
 
-type UserPrefs = Infer<typeof userPrefsSchema>
+type UserPrefs = Infer<typeof userPrefsStruct>
+
+const defaultUserPrefs: UserPrefs = {
+	showMissedFirst: true,
+	showClue: {
+		normalAchievement: {
+			beforeAchieved: true,
+			afterAchieved: true,
+		},
+		secretAchievement: {
+			beforeAchieved: false,
+			afterAchieved: true,
+		},
+	},
+}
 
 const userPrefsCookie = createCookie("user-prefs")
 
-async function getUserPrefs(request: Request): Promise<UserPrefs> {
+async function getUserPrefs(request: Request) {
 	const cookieHeader = request.headers.get("Cookie")
 	const cookie = await userPrefsCookie.parse(cookieHeader)
-	return mask(cookie ?? {}, userPrefsSchema)
+
+	if (!cookie) {
+		return defaultUserPrefs
+	}
+
+	try {
+		return mask(
+			{ showClue: { normalAchievement: { beforeAchieved: true } } },
+			userPrefsStruct
+		)
+	} catch (error) {
+		if (error instanceof StructError) {
+			let hasUnexpectedFailure = false
+			const failures = error.failures()
+			failures.forEach((failure) => {
+				if (is(failure.key, enums(["showMissedFirst", "showClue"]))) {
+					cookie[failure.key] = defaultUserPrefs[failure.key]
+				} else if (
+					is(failure.key, enums(["normalAchievement", "secretAchievement"]))
+				) {
+					cookie.showClue[failure.key] = defaultUserPrefs.showClue[failure.key]
+				} else {
+					hasUnexpectedFailure = true
+				}
+			})
+
+			if (hasUnexpectedFailure) {
+				return defaultUserPrefs
+			}
+			return cookie as UserPrefs
+		}
+		throw error
+	}
 }
 
+export type { UserPrefs }
 export { userPrefsCookie, getUserPrefs }
