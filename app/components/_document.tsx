0408cf51b357f275f6d539c84880dc901444d2c8
Jody Geraldo <jody.geraldo.jg@gmail.com>
2023-05-18 13:30:47 +0700

    replace select navigation with `go back` link on mobile display for settings page
    the select navigation was causing issues on the settings page where the selected option would remain active and prevent navigation

--- app/components/_document.tsx
+++ app/components/_document.tsx
@@ -99,6 +99,8 @@ function Sidebar() {
 		"root"
 	) as RootLoaderData
 
+	const location = useLocation()
+
 	return (
 		<div className="w-full self-start bg-gray-2 py-6 shadow-sm shadow-overlay-3 sm:rounded-lg lg:sticky lg:top-12 lg:max-w-md">
 			<div className="px-4">
@@ -110,6 +112,7 @@ function Sidebar() {
 					<Link
 						to="/settings"
 						prefetch="intent"
+						state={location.pathname}
 						className="rounded-md text-gray-11 hover:text-gray-12 focus-visible:outline focus-visible:outline-2 focus-visible:outline-gold-8"
 					>
 						<span className="sr-only">Settings</span>
@@ -131,7 +134,7 @@ function Sidebar() {
 					</Link>
 				</div>
 
-				<div className="mt-2 text-sm tabular-nums text-gray-12">
+				<div className="text-sm tabular-nums text-gray-12">
 					<span className="font-medium">{achievedTotal}</span>/{achievementSize}{" "}
 					&middot;{" "}
 					<span className="font-medium">
@@ -143,7 +146,7 @@ function Sidebar() {
 				</div>
 			</div>
 
-			<div className="mt-8">
+			<div className="mt-6">
 				<MobileNavigation />
 				<DesktopNavigation />
 			</div>
@@ -213,6 +216,40 @@ function MobileNavigation() {
 	const { categories } = useRouteLoaderData("root") as RootLoaderData
 	const location = useLocation()
 	const navigate = useNavigate()
+
+	const goBackTo =
+		typeof location.state === "string" && location.state !== location.pathname
+			? location.state
+			: "/trailblazer"
+
+	if (location.pathname === "/settings") {
+		return (
+			<div className="px-4 sm:hidden">
+				<Link
+					to={goBackTo}
+					prefetch="intent"
+					className="inline-flex items-center rounded-md text-sm font-medium text-gray-12 underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-gold-8"
+				>
+					<svg
+						width="15"
+						height="15"
+						viewBox="0 0 15 15"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						className="mr-0.5"
+					>
+						<path
+							d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
+							fill="currentColor"
+							fillRule="evenodd"
+							clipRule="evenodd"
+						/>
+					</svg>
+					Go back
+				</Link>
+			</div>
+		)
+	}
 
 	return (
 		<div className="px-4 sm:hidden">
