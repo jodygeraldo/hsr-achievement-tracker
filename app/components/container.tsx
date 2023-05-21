9209fb5fb8fbd57d6f5fd26f03c1cca7375176bd
Jody Geraldo <jody.geraldo.jg@gmail.com>
2023-05-21 19:57:55 +0700

    update main container

---
+++ app/components/container.tsx
@@ -0,0 +1,27 @@
+import * as React from "react"
+import { cn } from "~/utils/shared"
+
+type MainContainerProps = {
+	withAside?: boolean
+	children: React.ReactNode
+}
+
+function MainContainer({ withAside, children }: MainContainerProps) {
+	return (
+		<main className="lg:pl-96">
+			<div className={cn(withAside && "xl:pr-96")}>
+				<div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">{children}</div>
+			</div>
+		</main>
+	)
+}
+
+function AsideContainer({ children }: { children: React.ReactNode }) {
+	return (
+		<aside className="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-6 px-4 py-6 sm:px-6 lg:px-8 xl:block">
+			{children}
+		</aside>
+	)
+}
+
+export { MainContainer, AsideContainer }
