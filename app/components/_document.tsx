15deaa233d02313456c8f1afa0eaf931eda9e50a
Jody Geraldo <jody.geraldo.jg@gmail.com>
2023-05-26 15:43:51 +0700

    move assest to root dir

--- app/components/_document.tsx
+++ app/components/_document.tsx
@@ -33,21 +33,21 @@ function Document({ children }: { children: React.ReactNode }) {
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
-					href="/assets/apple-touch-icon.png"
+					href="/apple-touch-icon.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="32x32"
-					href="/assets/favicon-32x32.png"
+					href="/favicon-32x32.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="16x16"
-					href="/assets/favicon-16x16.png"
+					href="/favicon-16x16.png"
 				/>
-				<link rel="manifest" href="/assets/site.webmanifest" />
+				<link rel="manifest" href="/site.webmanifest" />
 				<Links />
 			</head>
 			<body className="h-full">
