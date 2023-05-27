ea030e1e2a6396074310310e386b30ffcc381f17
Jody Geraldo <jody.geraldo.jg@gmail.com>
2023-05-27 18:07:01 +0700

    update containers

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -3,14 +3,21 @@ import { cn } from "~/utils/shared"
 
 type MainContainerProps = {
 	withAside?: boolean
+	withPaddings?: boolean
 	children: React.ReactNode
 }
 
-function MainContainer({ withAside, children }: MainContainerProps) {
+function MainContainer({
+	withAside,
+	withPaddings = true,
+	children,
+}: MainContainerProps) {
 	return (
 		<main className="lg:pl-96">
-			<div className={cn(withAside && "xl:pr-96")}>
-				<div className="px-4 py-6 sm:px-6 lg:px-8">{children}</div>
+			<div className={cn(withAside && "xl:pr-80")}>
+				<div className={cn(withPaddings && "px-4 py-6 sm:px-6 lg:px-8")}>
+					{children}
+				</div>
 			</div>
 		</main>
 	)
@@ -18,7 +25,7 @@ function MainContainer({ withAside, children }: MainContainerProps) {
 
 function AsideContainer({ children }: { children: React.ReactNode }) {
 	return (
-		<aside className="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-6 bg-gray-2 px-4 py-6 sm:px-6 lg:px-8 xl:block">
+		<aside className="fixed inset-y-0 right-0 hidden w-80 overflow-y-auto border-l border-gray-6 bg-gray-2 px-4 py-6 sm:px-6 lg:px-8 xl:block">
 			{children}
 		</aside>
 	)
