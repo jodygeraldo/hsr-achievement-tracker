e2c87869871df30fc43f0211403eb4560db0dec4
Jody Geraldo <jody.geraldo.jg@gmail.com>
2023-05-30 16:09:05 +0700

    fix: persist user sessions on mobile devices by setting `maxAge` on cookie to prevent reset on browser close (#8)
    * set maxage of cookie to 6 monts
    
    * set maxage of cookie to 6 monts
    
    * chore: format

--- app/utils/user-prefs.server.ts
+++ app/utils/user-prefs.server.ts
@@ -40,7 +40,9 @@ const defaultUserPrefs: UserPrefs = {
 	},
 }
 
-const userPrefsCookie = createCookie("user-prefs")
+const userPrefsCookie = createCookie("user-prefs", {
+	maxAge: 15_768_000, // 6 months
+})
 
 async function getUserPrefs(request: Request) {
 	const cookieHeader = request.headers.get("Cookie")
