67f2a81573b1870b04e2e25074d44b34842fa134
Jody Geraldo <jody.geraldo.jg@gmail.com>
2023-05-30 15:47:41 +0700

    change input type of session id to `password`

--- app/routes/settings.sessions/route.tsx
+++ app/routes/settings.sessions/route.tsx
@@ -587,7 +587,7 @@ function ImportSessionForm() {
 					<FormLabel>Session ID</FormLabel>
 					<div className="mt-2">
 						<FormControl
-							type="text"
+							type="password"
 							className="block w-full rounded-md border-0 bg-gray-3 py-1.5 text-gray-12 shadow-sm ring-1 ring-inset ring-gray-6 focus:ring-2 focus:ring-inset focus:ring-gold-8 sm:text-sm sm:leading-6"
 							required
 						/>
